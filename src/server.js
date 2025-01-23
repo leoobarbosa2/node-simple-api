4bc0e20a85af314c80a5f65cf9b1887f88e0af2c
Leonardo Barbosa <leonardo.pb@icloud.com>
2025-01-23 09:46:16 -0300

    feat: working with buffer, converting to json and saving user in memory

--- src/server.js
+++ src/server.js
@@ -1,7 +1,31 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
-const server = http.createServer((req, res) => {
-    return res.end('Hello World!')
+const users = []
+
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req
+
+    await json(req, res)
+    
+    if (method === 'GET' && url === '/users') {
+        console.log(req.headers)
+        return res.end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+        const { name, email } = req.body
+
+        users.push({
+            id: 1,
+            name,
+            email
+        })
+
+        return res.writeHead(201).end('Created')
+    }
+
+    return res.writeHead(404).end('Users not found!')
 })
 
 server.listen(3333)
