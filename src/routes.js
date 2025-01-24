5c2899a282ca2df97871b64659f86b68f6578085
Leonardo Barbosa <leonardo.pb@icloud.com>
2025-01-23 21:47:30 -0300

    feat: editing user, deleting user and handling with query params

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,13 @@ export const routes = [
     method: 'GET',
     path: buildRoutePath('/users'),
     handler: (req, res) => {
-      const users = database.select('users')
+      const { search } = req.query
+
+      const users = database.select('users', search ? {
+        name: search,
+        email: search
+      }: null)
+      
       return res.end(JSON.stringify(users))
     }
   },
@@ -31,7 +37,25 @@ export const routes = [
     method: 'DELETE',
     path: buildRoutePath(`/users/:id`),
     handler: (req, res) => {
-      return null
+      const { id } = req.params
+
+      database.delete('users', id)
+      return res.writeHead(204).end()
     }
+ },
+ {
+  method: 'PUT',
+  path: buildRoutePath(`/users/:id`),
+  handler: (req, res) => {
+    const { id } = req.params
+    const { name, email } = req.body
+
+    database.update('users', id, { 
+      name, 
+      email
+    })
+
+    return res.writeHead(204).end()
   }
+}
 ]
